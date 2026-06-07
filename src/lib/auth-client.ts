03b9d89fa5db763905d4fa27d343df7dfc153a6b
Sojib-Ahmed07 <ahmed.sojib0456@gmail.com>
2026-06-07 11:29:05 +0600

    login fix 3

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,10 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  // 🚀 Look for the deployed server URL first, fallback to local port 5000 if developing locally
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000",
-  
+  fetchOptions: {
+    credentials: "include", // ← add this
+  },
   user: {
     additionalFields: {
       role: {
